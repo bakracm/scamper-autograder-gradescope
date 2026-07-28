fe3b562c377f4bdc82f8605984e0c28d9a41b5a1
Cade <johnston2@grinnell.edu>
2026-07-28 15:09:05 -0500

    Modifications from PM

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ apt-get update -y
 # chat thinks we should also do ca-certificates as a fix for https but we'll circle back
 apt-get install nodejs npm git -y
 
+# Update Things
+curl -fsSL https://nodesource.com | sudo -E bash -
+sudo apt-get install -y nodejs
+
 # clone scamper repo in folder
 git clone https://github.com/slag-plt/scamper.git /autograder/scamper
 cd /autograder/scamper
